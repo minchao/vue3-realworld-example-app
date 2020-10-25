bb02df72dcd25d820482ac326cfc9f9e74f658d1
levchak0910 <37511777+levchak0910@users.noreply.github.com>
2020-10-25 02:05:08 +0000

    Update gh-pages to output generated at d4b2f5b

--- _assets/getArticle.2fe5ab4b.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./index.5b037c0e.js";async function t(t){return e.get("/articles/"+t).then(e=>e.article)}export{t as g};
---
+++ _assets/getArticle.a48a1ba5.js
@@ -0,0 +1,1 @@
+import{e}from"./index.70321897.js";async function t(t){return e.get("/articles/"+t).then(e=>e.article)}export{t as g};
