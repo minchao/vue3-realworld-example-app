697706511fbe57898786ccf62f968e9ead3fd05a
mutoe <mutoe@foxmail.com>
2020-10-26 13:08:36 +0000

    Update gh-pages to output generated at 4c270e4

---
+++ _assets/getArticle.29bcf0ae.js
@@ -0,0 +1,1 @@
+import{g as t}from"./index.c919f844.js";async function e(e){return t.get("/articles/"+e).then((t=>t.article))}export{e as g};
--- _assets/getArticle.e2b04d71.js
+++
@@ -1,1 +0,0 @@
-import{g as t}from"./index.7aa046b2.js";async function a(a){return t.get("/articles/"+a).then(t=>t.article)}export{a as g};
