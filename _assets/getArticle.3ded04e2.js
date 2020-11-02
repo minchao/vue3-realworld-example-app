dede9ebd11b86053e2c72304be8235ec9e291b47
levchak0910 <37511777+levchak0910@users.noreply.github.com>
2020-11-02 15:50:59 +0000

    Update gh-pages to output generated at 33b776e

--- _assets/getArticle.3ded04e2.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index.ae550b03.js";async function r(r){return e.get("/articles/"+r).then((e=>e.article))}export{r as g};
---
+++ _assets/getArticle.d3278933.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.f0c3ed79.js";async function r(r){return e.get("/articles/"+r).then((e=>e.article))}export{r as g};
