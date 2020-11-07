4db3f5766c9094227250e7f2a33fd282da796d17
mutoe <mutoe@foxmail.com>
2020-11-07 05:36:39 +0000

    Update gh-pages to output generated at 7bc13fb

---
+++ _assets/getArticle.4a814093.js
@@ -0,0 +1,1 @@
+import{r}from"./index.1c7fa7fb.js";async function t(t){return r.get("/articles/"+t).then((r=>r.article))}export{t as g};
--- _assets/getArticle.c684bf1b.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index.d1393869.js";async function t(t){return r.get("/articles/"+t).then((r=>r.article))}export{t as g};
