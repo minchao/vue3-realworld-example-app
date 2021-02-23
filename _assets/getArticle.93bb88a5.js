98aa554671288ea80e58d59754f50c0b961e6bf7
mutoe <mutoe@foxmail.com>
2021-02-23 17:16:38 +0000

    Update gh-pages to output generated at 0dd3feb

--- _assets/getArticle.93bb88a5.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index.efddc1f7.js";function r(r){return e.get("/articles/"+r).then((e=>e.article))}export{r as g};
---
+++ assets/getArticle.186090aa.js
@@ -0,0 +1,1 @@
+import{r}from"./index.c52661b0.js";function t(t){return r.get(`/articles/${t}`).then((r=>r.article))}export{t as g};
