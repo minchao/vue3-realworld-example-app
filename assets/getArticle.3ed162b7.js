550b08c5f28a07b99d93a93faceb1f3215b17ca3
mutoe <mutoe@users.noreply.github.com>
2021-02-28 18:45:49 +0000

    Update gh-pages to output generated at bea80d8

---
+++ assets/getArticle.3ed162b7.js
@@ -0,0 +1,1 @@
+import{r}from"./index.35967ba7.js";function t(t){return r.get(`/articles/${t}`).then((r=>r.article))}export{t as g};
--- assets/getArticle.73cc1661.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index.5f2bac69.js";function t(t){return r.get(`/articles/${t}`).then((r=>r.article))}export{t as g};
